8b8cab0017576105f26a9244dbace31a68068940
Colin <colinclissonhg@gmail.com>
2025-04-20 12:29:44 -0400

    feat: cleanup

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Fakeformance",
+  description: "Create fake performance reports",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+});
+</script>
+
 <template>
   <NuxtPage />
 </template>
